e1a226572053081e0bf33c3c9ed78b36a59f3fc3
Daniel <danielsfskim@gmail.com>
2017-03-31 18:20:24 +0000

    built the about page using gbootstrap grid system

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'quotes#index'
   resources :quotes
+  get 'about', to: 'quotes#about' #get the about URL and point it to our about action inside our quotes controller
 end
